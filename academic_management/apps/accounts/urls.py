95a60996a7024bb4b1c7d1aef6286935e9d27f04
34codezak <zacharyz4qke@gmail.com>
2026-04-02 07:09:38 -0500

    Used the get_user_model function to reference the custom User model

--- academic_management/apps/accounts/urls.py
+++ academic_management/apps/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView  # Use Django's built-in logout
 
 # app_name = 'accounts' # Allows URL namespacing e.g., 'accounts:login'
 
@@ -9,5 +10,6 @@ urlpatterns = [
     path('verify/<uidb64>/<token>/', views.verify_email, name='verify_email'),
     path('resend-verification/', views.resend_verification, name='resend_verification'),
     path('login/', views.login_view, name='login'),
-    path('logout/', views.register_view, name='register'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    # path('logout/', views.register_view, name='logout'),
 ]
--- academic_management/apps/accounts/views.py
+++ academic_management/apps/accounts/views.py
@@ -8,7 +8,10 @@ from django.conf import settings
 from django.utils.http import urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
-from .models import User
+# from .models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model() # This now references your custom User
 
 def register_view(request):
     if request.method == "POST":
